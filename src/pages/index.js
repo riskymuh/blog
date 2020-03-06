6fbec367790eb3c83869df37e0d56acf7ba7956d
Riskymuhamad <42082391+Riskymuhamad@users.noreply.github.com>
2020-03-06 15:38:18 +0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,7 +25,9 @@ export default class Index extends Component {
       <Layout>
         <Helmet title={`${config.siteTitle} – Penulis Biasa`} />
         <SEO />
-    <h1>Halo, Risky Muhamad</h1>
+      <div class="container">
+     <div class="elevator">
+      <h1>Halo, Risky Muhamad</h1>
 
            <p>Situs ini berisi tulisan, opini dan juga merupakan dokumentasi untuk saya pribadi ketika sedang belajar atau membagikan sesuatu. Saya sengaja membebaskan situs ini dari iklan karena saya sendiri sering merasa terganggu dengan adanya iklan. Enjoy your reading! 😁</p>
 
