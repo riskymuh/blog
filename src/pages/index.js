0f417887c744b2d4a4cf161ae83dc96b6e4c4b46
riskymuhamad <riskymuhamad11@gmail.com>
2020-03-06 17:10:40 +0700

    1

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,7 +25,17 @@ export default class Index extends Component {
       <Layout>
         <Helmet title={`${config.siteTitle} – Penulis Biasa`} />
         <SEO />
-    <div class="container"><div class="lead"><div class="elevator"><h1>Halo, Saya Teguh Aprianto</h1><p>Situs ini berisi tulisan, opini dan juga merupakan dokumentasi untuk saya pribadi ketika sedang belajar atau membagikan sesuatu. Saya sengaja membebaskan situs ini dari iklan karena saya sendiri sering merasa terganggu dengan adanya iklan. Enjoy your reading! 😁</p><a class="selengkapnya" href="/about">Selengkapnya tentang Teguh Aprianto</a></div></div></div>
+<div class="container">
+<div class="lead">
+<div class="elevator">
+<h1>Halo, Saya Risky Muhamad</h1>
+<p>Situs ini berisi tulisan, opini dan juga merupakan dokumentasi untuk saya pribadi ketika sedang belajar atau membagikan sesuatu. Saya sengaja membebaskan situs ini dari iklan karena saya sendiri sering merasa terganggu dengan adanya iklan. Enjoy your reading! 😁</p>
+<a class="view-all" href="/me">Selengkapnya tentang Risky Muhamad</a>
+</div>
+<div class="elevator">
+</div>
+</div>
+</div>
 
         <div className="container front-page">
           <section className="section">
