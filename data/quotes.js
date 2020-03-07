2587cebc4b343e8b1e6384924330c195966c7e04
riskymuhamad <riskymuhamad11@gmail.com>
2020-03-07 18:23:07 +0700

    Update quote

--- data/quotes.js
+++ data/quotes.js
@@ -1,29 +1,18 @@
 export default [
   {
-    name: 'Craig',
-    quote: `It's no exaggeration to say that I
-  wouldn't currently have a job in development without this site. So thanks for
-  ruining my life, Tania.`,
+    name: 'Syekh Abdul Qadiri',
+    quote: `Aku lebih menghargai orang yang beradab dari pada berilmu. Kalau hanya berilmu, iblis pun lebih tinggi ilmunya dari pada manusia.`,
   },
   {
-    name: 'Lori',
-    quote: `Not to get too dramatic, but I find your site to be an unspeakably beautiful
-  lifeboat in an overwhelming sea of technical jargon and shite.`,
+    name: 'Kunto Hartono',
+    quote: `Setinggi Apapun Ilmu Duniamu. Sedalam Apapun Ilmu Agamamu. Jika Akhlaq, Adab & Etikamu Nyungsep. Kelak Suatu Saat Akan Malu & Dipermalukan.`,
   },
   {
-    name: 'Evan',
-    quote: `You taught me more than any class could have and it took me a fraction of the time because of how clearly you write and teach.`,
+    name: 'Jeff Bezos',
+    quote: `Saya tahu jika saya gagal, saya tidak akan menyesalinya. Tetapi saya tahu satu hal yang mungkin saya sesali adalah tidak mencobanya.`,
   },
   {
-    name: 'Zuzana',
-    quote: `I keep coming back to your website and I appreciate it more and more every time. The way you explain things just make sense!`,
-  },
-  {
-    name: 'P.P.',
-    quote: `You are the best tutor I have ever seen in my life. This tutorial is an excellent example of simplicity, clear explanations and detailed examples. I love every bit of your work. Never stop teaching, please.`,
-  },
-  {
-    name: 'Antony',
-    quote: 'Just wanted to thank you for your awesome tutorials. Glad you found your niche, teaching valuable skills to so many folks out there, instead of preparing fine meals for a select few.'
+    name: 'Raditya Dika',
+    quote: `financial freedom? Menunda kepuasan sekarang, untuk kepuasan yang lebih baik dimasa depan`,
   }
 ]
