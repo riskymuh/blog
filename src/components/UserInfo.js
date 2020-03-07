1c439de8ca0df0c82d5b480df9faa798eaf82e07
riskymuhamad <riskymuhamad11@gmail.com>
2020-03-07 20:51:22 +0700

    UserInfow2

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -3,147 +3,17 @@ import React, { Component } from 'react'
 export default class UserInfo extends Component {
   render() {
     return (
-
+      <aside className="note">
         <div className="container note-container">
         
-   <div class="tdb-author-box td_block_wrap tdb_single_author_box tdi_68_e53 tdb-content-vert-center td-pb-border-top td_block_template_4" data-td-block-uid="tdi_68_e53">
-<style>
-
-/* inline tdc_css att */
-
-.tdi_68_e53{
-margin-right:300px !important;
-margin-left:300px !important;
-}
-
-/* landscape */
-@media (min-width: 1019px) and (max-width: 1140px)
-{
-.tdi_68_e53{
-margin-right:150px !important;
-margin-left:150px !important;
-}
-}
-
-/* portrait */
-@media (min-width: 768px) and (max-width: 1018px)
-{
-.tdi_68_e53{
-margin-right:100px !important;
-margin-left:100px !important;
-}
-}
-
-/* phone */
-@media (max-width: 767px)
-{
-.tdi_68_e53{
-margin-right:10px !important;
-margin-left:10px !important;
-}
-}
-
-</style>
-<style>
-/* custom css */
-.tdi_68_e53{
-                    padding: 10px;
-                
-                    border: 1px solid #ededed;
-                }.tdi_68_e53 .tdb-author-info{
-                    width: auto;
-                
-                    padding-bottom: 0;
-                    padding-left: 15px;
-                }.tdi_68_e53 .tdb-author-photo,
-                .tdi_68_e53 .tdb-author-info{
-                    vertical-align: middle;
-                }.tdi_68_e53 .tdb-author-photo{
-                    width: 96px;
-                
-                    transform: translateZ(0);
-                    -webkit-transform: translateZ(0);
-                
-                    pointer-events: auto;
-                }.tdi_68_e53 .avatar,
-                .tdi_68_e53 .tdb-author-photo:before,
-                .tdi_68_e53 .tdb-author-photo:after{
-                    border-radius: 50px;
-                }.tdi_68_e53 .tdb-author-name{
-                    margin: 5px 0 8px 0;
-                
-					font-family:Source Sans Pro !important;font-size:18px !important;font-weight:600 !important;
-				}.tdi_68_e53 .tdb-social-item i{
-					font-size: 15px;
-					vertical-align: middle;
-				
-					line-height: 15px;
-				}.tdi_68_e53 .tdb-social-item i.td-icon-twitter,
-				.tdi_68_e53 .tdb-social-item i.td-icon-linkedin,
-				.tdi_68_e53 .tdb-social-item i.td-icon-pinterest,
-				.tdi_68_e53 .tdb-social-item i.td-icon-blogger,
-				.tdi_68_e53 .tdb-social-item i.td-icon-vimeo{
-					font-size: 12px;
-				}.tdi_68_e53 .tdb-social-item{
-					min-width: 15px;
-					height: 15px;
-				
-				    margin: 7px 14px 7px 0;
-				}.tdi_68_e53 .tdb-author-url{
-					font-family:Source Sans Pro !important;font-size:12px !important;
-				}.tdi_68_e53 .tdb-author-descr{
-					font-family:Source Sans Pro !important;font-size:15px !important;
-				}.tdi_68_e53 .tdb-author-photo:hover:before{
-                    opacity: 0;
-                }
-
-/* landscape */
-@media (min-width: 1019px) and (max-width: 1140px){
-.tdi_68_e53{
-                    border: 1px solid #ededed;
-                }.tdi_68_e53 .tdb-social-item{
-					border-radius: 50px;
-				}
-}
-
-/* portrait */
-@media (min-width: 768px) and (max-width: 1018px){
-.tdi_68_e53{
-                    border: 1px solid #ededed;
-                }
-}
-
-/* phone */
-@media (max-width: 767px){
-.tdi_68_e53{
-                    border: 1px solid #ededed;
-                }.tdi_68_e53 .tdb-author-info{
-                    padding-bottom: 0;
-                    padding-left: 10px;
-                }.tdi_68_e53 .tdb-author-photo{
-                    transform: translateZ(0);
-                    -webkit-transform: translateZ(0);
-                }
-}
-</style>
-      <div class="tdb-block-inner td-fix-index">
-      <a href="https://koinworks.com/blog/author/nimasdesaristanti/" class="tdb-author-photo">
-      <img src="https://secure.gravatar.com/avatar/9db62fa5dc7906407234cd93c4966106?s=150&amp;d=https%3A%2F%2Fblogkoinworks.sgp1.digitaloceanspaces.com%2F2016%2F05%2Ffavicon-150x150.png&amp;r=g" alt="Avatar" class="avatar avatar-150 wp-user-avatar wp-user-avatar-150 photo avatar-default" width="150" height="150"></a>
-      <div class="tdb-author-info">
-          <a href="https://koinworks.com/blog/author/nimasdesaristanti/" class="tdb-author-name">Nimas Des Aristanti</a>
-      <a href="" target="_blank" class="tdb-author-url"></a>
-      <div class="tdb-author-descr">Take a chance and never stop swimming. I'm here with my goals.</div>
-      <div class="tdb-author-social">
-     <a href="https://www.instagram.com/nimassz/" target="_blank" title="instagram" class="tdb-social-item"><i class="td-icon-font td-icon-instagram"></i></a>
-    <a href="https://id.linkedin.com/in/nimasarista" target="_blank" title="linkedin" class="tdb-social-item"><i class="td-icon-font td-icon-linkedin"></i></a>
-      </div>
-      </div>
-      </div>
-      </div>
+            <div>
+              <p>
+                Terima kasih telah membaca dan berkunjung ke situs ini
+              </p>
           
           </div>
         </div>
-  
+      </aside>
     )
   }
 }
