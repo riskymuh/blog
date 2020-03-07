ab20517da60f4a9244bf5c15cca16e99812b3c87
riskymuhamad <riskymuhamad11@gmail.com>
2020-03-07 20:28:42 +0700

    bawah2237

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -5,13 +5,13 @@ export default class UserInfo extends Component {
     return (
       <aside className="note">
         <div className="container note-container">
-          <div className="flex-author">
+        
             <div>
               <p>
                 I’m Risky Muhamad. I document everything I learn and help thousands of people start
                 coding careers.{' '}
               </p>
-            </div>
+          
           </div>
         </div>
       </aside>
