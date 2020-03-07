734fc6c3461002364ca6b5745387a276b27a07eb
riskymuhamad <riskymuhamad11@gmail.com>
2020-03-07 20:13:53 +0700

    bawah2

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,7 +1,4 @@
 import React, { Component } from 'react'
-import tania from '../../content/images/profile.jpg'
-import patreon from '../../content/thumbnails/patreon.png'
-import kofi from '../../content/thumbnails/kofi.png'
 
 export default class UserInfo extends Component {
   render() {
@@ -10,15 +7,14 @@ export default class UserInfo extends Component {
         <div className="container note-container">
           <div className="flex-author">
             <div className="flex-avatar">
-              <img className="avatar" src={tania} alt="Tania Rascia" />
-            </div>
-//            <div>
+
+
+
               <p>
                 I’m Risky Muhamad. I document everything I learn and help thousands of people start
                 coding careers.{' '}
               </p>
-//            </div>
-          </div>
+
         </div>
       </aside>
     )
