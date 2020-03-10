8ec24a55be04e184ce2f4611dc68c72cbcce837a
riskymuhamad <riskymuhamad11@gmail.com>
2020-03-10 16:16:10 +0700

    postingan perdana

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -82,6 +82,11 @@ export default class SEO extends Component {
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
         <meta property="og:image" content={image} />
+        <meta property="og:url"                content="http://www.nytimes.com/2015/02/19/arts/international/when-great-minds-dont-think-alike.html" />
+<meta property="og:type"               content="article" />
+<meta property="og:title"              content="When Great Minds Don’t Think Alike" />
+<meta property="og:description"        content="How much does culture influence creative thinking?" />
+<meta property="og:image"              content="http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg" />
 
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:creator" content={config.userTwitter} />
