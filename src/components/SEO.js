17d53da910c8cc2cc2ecf04164da3f824a80326c
Riskymuhamad <42082391+Riskymuhamad@users.noreply.github.com>
2020-03-10 17:48:06 +0700

    SEO

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Helmet from 'react-helmet'
+import { useStaticQuery, graphql } from "gatsby"
 import urljoin from 'url-join'
 import config from '../../data/SiteConfig'
 
