1f40f62966325676fd98c6c679ec308994ad4bd7
riskymuhamad <riskymuhamad11@gmail.com>
2020-03-11 19:03:45 +0700

    redired

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,16 +1,47 @@
 import React, { Component } from 'react'
+import risky from '../../content/images/profile.jpg'
+import patreon from '../../content/thumbnails/patreon.png'
+import kofi from '../../content/thumbnails/kofi.png'
 
 export default class UserInfo extends Component {
   render() {
     return (
       <aside className="note">
         <div className="container note-container">
-        
+          <div className="flex-author">
+            <div className="flex-avatar">
+              <img className="avatar" src={risky} alt="Risky Muhamad" />
+            </div>
             <div>
               <p>
-                Terima kasih telah membaca di situs ini
+                I’m Risky Muhamad. I document everything I learn and help thousands of people start
+                coding careers.{' '}
+                <strong>
+                  My site has no ads, sponsors, or bullshit. If you enjoy my content, please
+                  consider supporting what I do.
+                </strong>
               </p>
-          
+
+              <div className="flex">
+                <a
+                  href="https://ko-fi.com/taniarascia"
+                  className="donate-button"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src={kofi} className="coffee-icon" alt="Coffee icon" />
+                  Buy me a coffee
+                </a>
+                <a
+                  className="patreon-button"
+                  href="https://www.patreon.com/taniarascia"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src={patreon} height="50" width="50" alt="Patreon" /> Become a Patron
+                </a>
+              </div>
+            </div>
           </div>
         </div>
       </aside>
